874b58bd88f1f7a2760ef2772abf0816202cf6e1
tesbuatdo <82263914+tesbuatdo@users.noreply.github.com>
2021-05-20 10:25:16 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,3 +1,5 @@
 sudo apt update
 sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt -y install nodejs
+sudo apt -y install gcc g++ make
