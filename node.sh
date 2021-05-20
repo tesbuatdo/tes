d8e43bb08afcfbff5ee61dc08bab9eb1d53974ea
tesbuatdo <82263914+tesbuatdo@users.noreply.github.com>
2021-05-20 10:56:51 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,5 +1,18 @@
 sudo apt update
 sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt -y install nodejs
 sudo apt -y install gcc g++ make
+## Confirming "xenial" is supported...
+
+curl -sLf -o /dev/null 'https://deb.nodesource.com/node_12.x/dists/xenial/Release'
+
+## Adding the NodeSource signing key to your keyring...
+
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | tee /usr/share/keyrings/nodesource.gpg >/dev/null
+
+## Creating apt sources list file for the NodeSource Node.js 12.x repo...
+
+echo 'deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_12.x xenial main' > /etc/apt/sources.list.d/nodesource.list
+echo 'deb-src [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_12.x xenial main' >> /etc/apt/sources.list.d/nodesource.list
+apt-get -y install nodejs
+apt-get -y install npm
